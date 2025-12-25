6773f4cbe311e0c8e997c716e18298450bd98d54
papettosunsun <8zz69ks6j8@privaterelay.appleid.com>
2025-12-25 15:31:38 +0900

    各ページに飛べる様にしました
    ページからトップに戻れる様にしたよ

--- app.py
+++ app.py
@@ -2,18 +2,25 @@ from flask import Flask, render_template
 
 app = Flask(__name__)
 
+# トップページ
 @app.route('/')
 def index():
     return render_template('index.html')
 
-# 仮でボタンリンク用のルートも作っておく
-@app.route('/add_shop')
-def add_shop():
-    return "<h2>お店登録ページ（まだフォームなし）</h2>"
+# お店登録ページ
+@app.route('/restaurant_registration')
+def restaurant_registration():
+    return render_template('restaurant_registration.html')
 
-@app.route('/shop_list')
-def shop_list():
-    return "<h2>お店一覧ページ（まだデータなし）</h2>"
+# お店一覧ページ
+@app.route('/restaurant_list')
+def restaurant_list():
+    return render_template('restaurant_list.html')
+
+# レビューページ
+@app.route('/review')
+def review():
+    return render_template('review.html')
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=5001, debug=True)
