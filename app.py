f1a617d747b4429ac36ea1108de856fa41ca8175
T-K-T-K-T <k24128kk@aitech.ac.jp>
2026-01-12 16:19:00 +0900

    Update app.py

--- app.py
+++ app.py
@@ -26,4 +26,4 @@ def restaurant_registration():
     return render_template('restaurant_registration.html')
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=5000, debug=True)
+    app.run(host='0.0.0.0', port=5001, debug=True)
